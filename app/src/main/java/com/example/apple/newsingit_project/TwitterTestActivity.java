8339ec080d78e291afe2fbc66e3897aa166ebbb4
limjs <lim_js91@naver.com>
2016-08-29 16:37:33 +0900

    [NewsingIT Project 2016/08/29 16:37] - Twitter timeline

--- app/src/main/java/com/example/apple/newsingit_project/TwitterTestActivity.java
+++ app/src/main/java/com/example/apple/newsingit_project/TwitterTestActivity.java
@@ -1,24 +1,21 @@
 package com.example.apple.newsingit_project;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.GridView;
+import android.widget.ListView;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
-import cn.iwgang.familiarrecyclerview.FamiliarRecyclerView;
 import io.fabric.sdk.android.Fabric;
 
-public class TwitterTestActivity extends AppCompatActivity {
+public class TwitterTestActivity extends Activity {
     private static final String TWITTER_KEY = "WKUT4AupkcyiAUHVVAM5xrIvE";
     private static final String TWITTER_SECRET = "yR9fxY13cGOJyG0vPMAu6a8HdDNOcU9FPczPncLm6OwyxdsPkM";
 
-    FamiliarRecyclerView recyclerView;
-
-    @Override
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -30,15 +27,16 @@ public class TwitterTestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_twitter_test);
 
         SearchTimeline searchTimeline = new SearchTimeline.Builder()
-                .query("#iot")
+                .query("#김수현")
                 .languageCode("ko")
+                .maxItemsPerRequest(200)
                 .build();
 
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
                 .setTimeline(searchTimeline)
                 .build();
 
-        GridView listview =(GridView) findViewById(R.id.testlist);
+        ListView listview =(ListView)findViewById(R.id.testlist);
 
         listview.setAdapter(adapter);
 
